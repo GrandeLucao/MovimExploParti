f7fff3837a22712b5386302149f537d24c1f2764
GrandeLucao <lucasgameronbrazil@gmail.com>
2024-09-24 13:13:22 -0300

    luz funcionando, arrumar collider

--- Curves/Assets/Scripts/Lamp.cs
+++ Curves/Assets/Scripts/Lamp.cs
@@ -4,43 +4,69 @@ using UnityEngine;
 
 public class Lamp : MonoBehaviour
 {
-    public AnimationCurve curve;
+    private Light m_light;
+    private Animation m_anim;
+    private AnimationClip m_clip;
+    public AnimationCurve m_curve;
     [SerializeField]
     private bool startAnim=false;
     private bool playaClose;
 
     public float duration;
+    public float initialIntensity;
+    private float intensityIncreaseValue;
 
     float elapsedTime;
-    Vector3 initalScale;
+    
 
     void Start()
-    {
-        initalScale=transform.localScale;
-        curve.preWrapMode=WrapMode.PingPong;
-        curve.postWrapMode=WrapMode.PingPong;
+    {   
+        m_light=GetComponent<Light>();
+        m_anim=GetComponent<Animation>();
+        m_clip=new AnimationClip();
+        m_clip.legacy = true;
+        m_light.intensity=initialIntensity;
+
+        m_curve.preWrapMode=WrapMode.PingPong;
+        m_curve.postWrapMode=WrapMode.PingPong;
+
+        m_clip.SetCurve("",typeof(Light),"intensity",m_curve);
+        m_anim.AddClip(m_clip,"Intensity");
         
     }
 
 
     void Update()
-    {
+    {        
         if(startAnim){
             playaClose=true;
             elapsedTime=0f;
-            startAnim=false;            
+            startAnim=false;  
+            intensityIncreaseValue=0.05f;          
         }
         AnimTrig();
     }
 
     void AnimTrig(){
         if(playaClose){
-            elapsedTime+=Time.deltaTime;
-            float scalingValue=curve.Evaluate(elapsedTime);
-            transform.localScale=new Vector3(initalScale.x+scalingValue,initalScale.y+scalingValue,initalScale.z+scalingValue);
+            elapsedTime+=Time.deltaTime;  
+            if(elapsedTime<duration){          
+                m_light.intensity+=intensityIncreaseValue;
+                m_anim.Play("Intensity");
+            } 
             if(elapsedTime>duration){
-                playaClose=false;
-            }
+                if(m_light.intensity>initialIntensity){
+                    m_light.intensity-=intensityIncreaseValue;
+                    m_anim.Play("Intensity");
+                }
+            }         
+        }
+    }
+
+    private void OnTriggerEnter(Collider coll){
+        if(coll.gameObject.tag=="Player"){
+            Debug.Log("nig");
+            startAnim=true;
         }
     }
 }
