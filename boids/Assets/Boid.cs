8b2de1b45eba4662a37ddb3c057379b178495019
GrandeLucao <lucasgameronbrazil@gmail.com>
2024-10-09 17:19:15 -0300

    falta resolver moimentacao

--- boids/Assets/Boid.cs
+++ boids/Assets/Boid.cs
@@ -4,35 +4,38 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
-    Vector2 position;
-    Vector2 velocity;
-    Vector2 acceleration;
+    Transform position;
+    Vector3 velocity;
+    Vector3 acceleration;
+    [SerializeField]
+    int ID;
 
     void Awake()
     {
         velocity.x = Random.Range(-1f, 1f);
         velocity.y = Random.Range(-1f, 1f);
+        ID=Random.Range(1,4);
     }
 
 
     void FixedUpdate()
     {
         velocity += acceleration * Time.fixedDeltaTime;
-        position += velocity * Time.fixedDeltaTime;
+        transform.position += velocity * Time.fixedDeltaTime;
     }
 
-    public Vector2 s_acceleration{
+    public Vector3 s_acceleration{
         get { return acceleration; }
         set { acceleration = value; }
     }
 
-    public Vector2 s_velocity{
+    public Vector3 s_velocity{
         get { return velocity; }
         set { velocity = value; }
     }
 
-    public Vector2 s_position{
-        get { return position; }
-        set { position = value; }
+    public int s_ID{
+        get { return ID; }
+        set { ID = value; }
     }
 }
--- boids/Assets/Flock.cs
+++ boids/Assets/Flock.cs
@@ -9,34 +9,120 @@ public class Flock : MonoBehaviour
     private void Start()
     {
         boids = FindObjectsByType<Boid>(FindObjectsSortMode.None);
+        bool definedHunter=false;
+        foreach(var boid in boids){
+            if(boid.s_ID!=4 && !definedHunter){
+                boid.s_ID=4;
+                definedHunter=true;
+            }else if(boid.s_ID==4){
+                boid.s_ID=Random.Range(1,3);
+            }
+        }
     }
 
     void FixedUpdate()
     {
+        for(int i=0;i<boids.Length;i++){
+            Separate();
+            //Align();
+            //Cohesion();
+            if(boids[i].s_ID==4){
+                Hunt();
+            }else{
+                Flee();
+            }
+        }
+    }
+
+    void Separate(){
         foreach (var boid in boids)
         {
-            Vector2 center = Vector2.zero;
+            Vector3 center = Vector3.zero;
             int count = 0;
             foreach (var other in boids)
             {
-                float distance = Vector2.Distance(other.s_position, boid.s_position);
+                float distance = Vector3.Distance(other.transform.position, boid.transform.position);
                 if (other != boid && distance < 1f)
                 {
-                    center += other.s_position;
+                    center += other.transform.position;
                     count++;
                 }
             }
             if (count == 0)
             {
-                center = boid.s_position;
+                center = boid.transform.position;
             }
             else
             {
                 center /= count;
             }
 
-            boid.s_acceleration = boid.s_position - center;
-            boid.s_acceleration += -boid.s_position * 0.2f;
+            boid.s_acceleration = boid.transform.position - center;
+            boid.s_acceleration += -boid.transform.position * 0.2f;
         }
+    }
+
+    void Align(){
+        foreach (var boid in boids)
+        {
+            Vector3 center = Vector3.zero;
+            int count = 0;
+            foreach (var other in boids)
+            {
+                float distance = Vector3.Distance(other.transform.position, boid.transform.position);
+                if (other.s_ID == boid.s_ID )
+                {
+                    center += other.transform.position;
+                    boid.s_velocity=other.s_velocity;
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                center = boid.transform.position;
+            }
+            else
+            {
+                center /= count;
+            }
+
+            boid.s_acceleration = boid.transform.position - center;
+            boid.s_acceleration += -boid.transform.position * 0.2f;
+        }
+    }
+
+    void Cohesion(){
+        foreach (var boid in boids)
+        {
+            Vector3 center = Vector3.zero;
+            int count = 0;
+            foreach (var other in boids)
+            {                
+                if (other.s_ID== boid.s_ID)
+                {
+                    center += other.transform.position;
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                center = boid.transform.position;
+            }
+            else
+            {
+                center /= count;
+            }
+
+            boid.s_acceleration = boid.transform.position - center;
+            boid.s_acceleration += boid.transform.position * 0.2f;
+        }
+    }
+
+    void Hunt(){
+
+    }
+
+    void Flee(){
+
     }
 }
